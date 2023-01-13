2d3b40ceb8592f7a83a29e7e1c50f380657711cc
timotsuihc <31647697+timotsuihc@users.noreply.github.com>
2023-01-12 23:02:25 -0800

    First commit for RhinoVoiceCommand
    Archive Folder

---
+++ RhinoVoiceCommand/Speech2/Form1.Designer.cs
@@ -0,0 +1,93 @@
+﻿namespace Speech_Tutorial_2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 224);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(242, 116);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Enable";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.button2.Location = new System.Drawing.Point(260, 224);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(152, 116);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Disable";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.richTextBox1.Location = new System.Drawing.Point(12, 27);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.ReadOnly = true;
+            this.richTextBox1.ScrollBars = System.Windows.Forms.RichTextBoxScrollBars.ForcedVertical;
+            this.richTextBox1.Size = new System.Drawing.Size(400, 191);
+            this.richTextBox1.TabIndex = 4;
+            this.richTextBox1.Text = "";
+            this.richTextBox1.TextChanged += new System.EventHandler(this.richTextBox1_TextChanged_1);
+            // 
+            // Form1
+            // 
+            this.AcceptButton = this.button1;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(424, 352);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Rhino Speech Command";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+    }
+}
+
---
+++ RhinoVoiceCommand/Speech2/Form1.cs
@@ -0,0 +1,227 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Speech.Recognition;
+using System.Threading;
+using System.IO;
+
+namespace Speech_Tutorial_2
+{
+    public partial class Form1 : Form
+    {
+
+        public SpeechRecognitionEngine recognizer;
+        public Grammar grammar;
+        public Thread RecThread;
+        public Boolean RecognizerState = true;
+        public List<string> rhinoCommands = new List<string>();
+        Choices commands = new Choices();
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            TopMost = true;
+            
+
+            String line;
+            try
+            {
+                //Pass the embeded file here to the StreamReader constructor
+                //StreamReader sr = new StreamReader(@"D:\Timo\Documents\GSD Yr IV\CS50\final\RhinoCommand.txt");
+                StringReader sr = new StringReader(Properties.Resources.RhinoCommand);
+
+                //Read the text
+                line = sr.ReadLine();
+
+                //Continue untill end of file
+                while (line != null)
+                {
+                    //make lower case
+                    //rhinoCommands.Add(line.ToLower());
+                    //commands.Add(line.ToLower());
+                    
+                    //Add the commands to a list
+                    commands.Add(line);
+
+                    //write the lien to console window
+                    //richTextBox1.Text += ( line.ToLower() + "\n");
+
+                    //Read the next line
+                    line = sr.ReadLine();
+                }
+
+
+
+                    sr.Close();
+
+                for (int ii = 0; ii < 1000; ii++)
+                {
+                    commands.Add(ii.ToString());
+                }              
+
+
+                return;
+            }
+            catch (Exception k)
+            {
+                richTextBox1.Text += (" " + k + "\n");
+            }
+            finally
+            {
+                //indicate its done
+                richTextBox1.Text += ("READY\n");
+            }
+        }
+        
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //Build the diction            
+            GrammarBuilder build = new GrammarBuilder();
+            //build.AppendDictation();
+            build.Append(commands);
+
+            //add the diction
+            grammar = new Grammar(build);
+ 
+            //turn on recognizer 
+            recognizer = new SpeechRecognitionEngine();
+            recognizer.LoadGrammar(grammar);
+            recognizer.SetInputToDefaultAudioDevice();
+
+            //find recognized event
+            recognizer.SpeechRecognized += new EventHandler<SpeechRecognizedEventArgs>(recognizer_SpeechRecognized);
+            //recognizer.SpeechHypothesized += new EventHandler<SpeechHypothesizedEventArgs>(recognizer_SpeechRecognized);
+
+            //initialize the recognizer thread
+            RecognizerState = true;
+            RecThread = new Thread(new ThreadStart(RecThreadFunction));
+            RecThread.Start();
+ 
+        }
+
+        public void recognizer_SpeechRecognized(object sender, SpeechRecognizedEventArgs e)
+        //public void recognizer_SpeechRecognized(object sender, SpeechHypothesizedEventArgs e)
+        {
+            //Recognizer recognizes
+            if (!RecognizerState)
+                return;
+            this.Invoke((MethodInvoker)delegate
+            {
+                //standarize the result
+                String Nresult = e.Result.Text.ToLower();
+                Nresult = Nresult.Replace(".", " ");
+                Nresult = Nresult.Trim();
+                
+                //if (rhinoCommands.Contains(e.Result.Text.ToLower()))
+                //if (rhinoCommands.Any(o => o.StartsWith(Nresult)))
+
+                //display and output the results
+                if (true)
+                {
+                    richTextBox1.Text += ( Nresult + "\n");
+                    //richTextBox1.AppendText(Nresult + "\n");
+
+                    //scroll to bottom
+                    richTextBox1.SelectionStart = richTextBox1.Text.Length;
+                    richTextBox1.ScrollToCaret();
+
+                    if ( Nresult == "enter")
+                    {
+                        SendKeys.Send("{ENTER}");
+                    }
+                                        
+                    else
+                    {
+                        int throwaway;
+                        if (Int32.TryParse(Nresult, out throwaway))
+                        {
+                            SendKeys.Send(Nresult);
+                            SendKeys.Send("{ENTER}");
+                        }
+                        else
+                        {
+                            SendKeys.Send("{ESC}");
+                            SendKeys.Send(Nresult);
+                            SendKeys.Send("{ENTER}");
+                        }
+                        
+                    }
+                    
+                }                  
+                
+                else
+                {
+                    richTextBox1.Text += ("invalide:" + Nresult + "\n");
+                }
+            
+            });
+        }
+
+        public void RecThreadFunction()
+        {
+            
+            while (true)
+            {
+                try
+                {
+                    recognizer.Recognize();
+                }
+                catch
+                {
+                    //handles errors
+                    //nothing happens
+                }
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //when enabled set top and display ready
+            RecognizerState = true;
+            TopMost = true;
+            richTextBox1.Text += ("READY\n");
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            //when disabled do the opposite
+            RecognizerState = false;
+            richTextBox1.Text += ("DISABLED\n");
+            TopMost = false;
+
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            //unload all memory
+            RecThread.Abort();
+            RecThread = null;
+            recognizer.UnloadAllGrammars();
+            recognizer.Dispose();
+            grammar = null;
+            for (int j = 0; j < rhinoCommands.Count; j++)
+            {
+                rhinoCommands[j] = null;
+            }
+            TopMost = false;
+
+        }
+
+        private void richTextBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void richTextBox1_TextChanged_1(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
